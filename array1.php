456832061b044053ffa011a945470382fd5440b0
SarahElizabeteTDS <130590547+SarahElizabeteTDS@users.noreply.github.com>
2024-02-29 15:26:25 -0300

    Update array1.php

--- array1.php
+++ array1.php
@@ -1,23 +1,19 @@
 <?php
 
+function imprimeVetor($vet)
+{
+    foreach ($vet as $nome) 
+    {
+        print $nome . "|";
+    }
+}
+
 $vetor = array("Walmonn", "Rebs", "Vini", "Vini", "Sarah");
 $vetor1 = array("Julio", "Daniel", "Jeffinho", "Silvia", "Marcelo");
 $vetor2 = array("Buche", "Ana Ju", "Kassime", "Mayara", "Leticia");
 $vetor3 = array("Jonathan", "Mari", "Rafael", "Yuri", "Pazinato");
 
-foreach ($vetor as $nome) 
-{
-    print $nome . "|";
-}
-foreach ($vetor1 as $nome) 
-{
-    print $nome . "|";
-}
-foreach ($vetor2 as $nome) 
-{
-    print $nome . "|";
-}
-foreach ($vetor3 as $nome) 
-{
-    print $nome . "|";
-}
+imprimeVetor($vetor);
+imprimeVetor($vetor1);
+imprimeVetor($vetor2);
+imprimeVetor($vetor3);
