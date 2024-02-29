6f526a5dcb861b1a6c73f6c3c79f5521bed32573
SarahElizabeteTDS <130590547+SarahElizabeteTDS@users.noreply.github.com>
2024-02-29 15:04:24 -0300

    Create array1.php

---
+++ array1.php
@@ -0,0 +1,23 @@
+<?php
+
+$vetor = array("Walmonn", "Rebs", "Vini", "Vini", "Sarah");
+$vetor1 = array("Julio", "Daniel", "Jeffinho", "Silvia", "Marcelo");
+$vetor2 = array("Buche", "Ana Ju", "Kassime", "Mayara", "Leticia");
+$vetor3 = array("Jonathan", "Mari", "Rafael", "Yuri", "Pazinato");
+
+foreach ($vetor as $nome) 
+{
+    print $nome . "|";
+}
+foreach ($vetor1 as $nome) 
+{
+    print $nome . "|";
+}
+foreach ($vetor2 as $nome) 
+{
+    print $nome . "|";
+}
+foreach ($vetor3 as $nome) 
+{
+    print $nome . "|";
+}
